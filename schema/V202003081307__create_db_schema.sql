d05d3b135975e7c59ef86efdfd2985be74e516d6
Jun Ye <jye@coupang.com>
2020-03-09 07:35:34 +0900

    fix typo in db schema script

--- schema/V202003081307__create_db_schema.sql
+++ schema/V202003081307__create_db_schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE pingpong;
 
 CREATE TABLE pingpong.player (
-  player_id INT(11) AUTO_INCREMENT
+  player_id INT(11) AUTO_INCREMENT,
   `name` INT(11) NOT NULL,
   login_id VARCHAR(50) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE pingpong.player (
 CREATE TABLE pingpong.single_match (
   single_match_id INT(11) AUTO_INCREMENT,
   sets_format VARCHAR(20) NOT NULL COMMENT 'BEST_OF_5 or BEST_OF_7',
-  referee VACHAR(50) NULL COMMENT 'Referee name',
+  referee VARCHAR(50) NULL COMMENT 'Referee name',
   start_time DATETIME(6) NOT NULL,
   end_time DATETIME(6) NULL,
   created_by VARCHAR(50) NOT NULL,
@@ -26,11 +26,11 @@ CREATE TABLE pingpong.single_match (
 );
 
 CREATE TABLE pingpong.single_game (
-  single_game_id INT(11) AUTO_INCREMENT
+  single_game_id INT(11) AUTO_INCREMENT,
   single_match_id INT(11) NOT NULL,
   seq SMALLINT NOT NULL COMMENT 'Sequence of the game in the match. Starts from 1. e.g. 1, 2, 3',
   sets_format VARCHAR(20) NOT NULL COMMENT 'BEST_OF_5 or BEST_OF_7',
-  referee VACHAR(50) NULL COMMENT 'Referee name',
+  referee VARCHAR(50) NULL COMMENT 'Referee name',
   start_time DATETIME(6) NOT NULL,
   end_time DATETIME(6) NULL,
   created_by VARCHAR(50) NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE pingpong.single_game (
 );
 
 CREATE TABLE pingpong.single_game_result (
-  single_game_result_id INT(11) AUTO_INCREMENT
+  single_game_result_id INT(11) AUTO_INCREMENT,
   single_game_id INT(11) NOT NULL,
   player_id INT(11) NOT NULL,
   score SMALLINT NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE pingpong.single_game_result (
 
 
 CREATE TABLE pingpong.single_participation (
-  single_participation_id INT(11) AUTO_INCREMENT
+  single_participation_id INT(11) AUTO_INCREMENT,
   single_match_id INT(11) NOT NULL,
   player_id INT(11) NOT NULL,
   created_by VARCHAR(50) NOT NULL,
